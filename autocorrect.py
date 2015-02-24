534c77afbb3a2bac7927988585777ce7e99ca427
Anurag Pallaprolu <anurag.pallaprolu@gmail.com>
2015-02-25 04:09:34 +0530

    latest commit

---
+++ autocorrect.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('wiki.jpg',0)
+
+hist,bins = np.histogram(img.flatten(),256,[0,256])
+
+cdf = hist.cumsum()
+cdf_normalized = cdf * hist.max()/ cdf.max()
+
+plt.plot(cdf_normalized, color = 'b')
+plt.hist(img.flatten(),256,[0,256], color = 'r')
+plt.xlim([0,256])
+plt.legend(('cdf','histogram'), loc = 'upper left')
+plt.show()
+
+img = cv2.imread('wiki.jpg',0)
+equ = cv2.equalizeHist(img)
+res = np.hstack((img,equ)) #stacking images side-by-side
+cv2.imwrite('res.png',res)
+
+img = cv2.imread('tsukuba_l.png',0)
+
+# create a CLAHE object (Arguments are optional).
+clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+cl1 = clahe.apply(img)
+
+cv2.imwrite('clahe_2.jpg',cl1)
