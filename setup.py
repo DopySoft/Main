f5c62ae57a493f47ccfbd7613ea3fe32727c87dd
adwait <f2011164@pilani.bits-pilani.ac.in>
2015-02-25 17:47:01 +0530

    Biller-Bissy Merge

---
+++ setup.py
@@ -0,0 +1,14 @@
+import sys
+
+from cx_Freeze import setup, Executable
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(
+        name = "simple_PyQt4",
+        version = "0.1",
+        description = "yolz",
+        executables = [Executable(r"C:\Users\Rohit\Desktop\soft work\new code\soft\fresh\mainWindow.py", base = base)])
+        #executables = [Executable(r"C:\Users\Rohit\Desktop\soft work\new code\soft\fresh\mainWindow.py")])
